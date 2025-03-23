a825e405e3f9ac8a86b904e34cce83b88bd3f5b5
DavizonS <deivisonortegasuarez@gmail.com>
2025-03-22 22:44:59 -0500

    Agregue y organise la estructura inicial del proyecto con componentes, estilos e imágenes

---
+++ Angular/laPizza/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { IniciosesionComponent } from './componentes/iniciosesion/iniciosesion.component';
+import { DisboardComponent } from './componentes/disboard/disboard.component';
+import { authGuard } from './auth.guard';
+import { TablaUsuarioComponent } from './componentes/tabla-usuario/tabla-usuario.component';
+import { IframesComponent } from './componentesPhp/iframes/iframes.component';
+import { ReservaComponent } from './componentes/reserva/reserva.component';
+
+export const routes: Routes = [
+<<<<<<< Updated upstream:laPizza/src/app/app.routes.ts
+    { path: '', component: FormularioComponent },
+    { path: 'inicio-sesion', component: IniciosesionComponent},
+    { path: 'disboard', component: DisboardComponent,canActivate: [authGuard]},
+    { path: 'tablaUsuario', component: TablaUsuarioComponent,canActivate: [authGuard]},
+    { path: 'iframes', component: IframesComponent,canActivate: [authGuard]},
+    { path: 'reserva', component: ReservaComponent},
+=======
+    { path: '', redirectTo: 'inicio-sesion', pathMatch: 'full' }, // 🔹 Cambiado
+    { path: 'inicio-sesion', component: IniciosesionComponent },
+    { path: 'disboard', component: DisboardComponent, canActivate: [authGuard] },
+    { path: 'tablaUsuario', component: TablaUsuarioComponent, canActivate: [authGuard] },
+    { path: 'iframes', component: IframesComponent, canActivate: [authGuard] },
+    { path: 'reserva', component: ReservaComponent },
+    { path: 'formulario', component: FormularioComponent }
+>>>>>>> Stashed changes:Angular/laPizza/src/app/app.routes.ts
+];
