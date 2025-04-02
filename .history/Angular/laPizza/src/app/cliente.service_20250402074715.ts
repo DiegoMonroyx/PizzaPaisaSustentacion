e3a0f659cf519c1b8357ee2a7edd4818d8869087
DomleLiz <lizeth2005castro18@gmail.com>
2025-04-02 07:47:44 -0500

    validacion en campos de registro

---
+++ .history/Angular/laPizza/src/app/cliente.service_20250402074715.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  private apiUrl = 'http://localhost:8000/api/pizzapaisa';
+  
+  constructor(private http: HttpClient) {}
+
+
+
+  getClientes(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+  getTiposUsuario() {
+    return this.http.get<any[]>(`${this.apiUrl}/tipos-usuario`);
+  }
+  getTiposDocumento() {
+    return this.http.get<any[]>(`${this.apiUrl}/tipos-documento`);
+  }
+
+  //updateCliente(usuario: any) {
+   // const url = `http://localhost:8000/api/pizzapaisa/${usuario.UsuarioDocumento}`;
+//return this.http.put(url, usuario);}
+}
