238eed9d52f723f6480c0d9370a3e16874e16a17
DiegoMonroyx <jmonroymurillo@gmail.com>
2025-05-22 20:44:10 -0500

    Revert "Merge pull request #57 from DiegoMonroyx/dockerprueba"
    This reverts commit 58e9e47c046fcf2bbf04c9e7a017ef39f59c40ff, reversing
    changes made to 4379ac292393d35b6feeea40ec0e8af5513db62e.

--- Angular/laPizza/dist/la-pizza/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-SGP2DX35.mjs";import"./chunk-7FKDPLIK.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
--- Angular/laPizza/dist/la-pizza/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Ma as e,lb as o,mb as r,nb as n}from"./chunk-7FKDPLIK.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
