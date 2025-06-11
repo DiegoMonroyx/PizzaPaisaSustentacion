47fe5805b4e8ec0bc7dc3be7ea56b6570763d002
Juan Diego Monroy Murillo <158363471+DiegoMonroyx@users.noreply.github.com>
2025-06-10 23:09:19 -0500

    Update TipoDocumentoControlador.php

--- PHP/PizzaPaisa/controlador/TipoDocumentoControlador.php
+++ PHP/PizzaPaisa/controlador/TipoDocumentoControlador.php
@@ -22,30 +22,10 @@ class TipoDocumentoControlador
 
     public function buscar($idTipoDocumento)
     {
-        $c = (new Conexion())->conectando();
-        $sql = "SELECT * FROM tipodocumento WHERE idTipoDocumento LIKE ?";
-        $stmt = $c->prepare($sql);
-        $like = "%$idTipoDocumento%";
-        $stmt->bind_param("s", $like);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $datos = [];
-        while ($row = $result->fetch_assoc()) {
-            $datos[] = $row;
-        }
-        $stmt->close();
-        return $datos;
+        return $this->model->buscar($idTipoDocumento);
     }
-
     public function listar()
     {
-        $c = (new Conexion())->conectando();
-        $sql = "SELECT * FROM tipodocumento";
-        $res = $c->query($sql);
-        $datos = [];
-        while ($row = $res->fetch_assoc()) {
-            $datos[] = $row;
-        }
-        return $datos;
+        return $this->model->listar();
     }
 }
