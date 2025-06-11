746a408cc9578aec5fe231b8940c88fa85e74971
Juan Diego Monroy Murillo <158363471+DiegoMonroyx@users.noreply.github.com>
2025-06-10 23:21:03 -0500

    Update environment.prod.ts

--- Angular/laPizza/src/environments/environment.prod.ts
+++ Angular/laPizza/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://20.55.34.81:8000/api',
-  baseUrl: 'http://20.55.34.81:8080'
+  apiUrl: 'https://20.55.34.81:8000/api',
+  baseUrl: 'https://20.55.34.81:8080'
 };
